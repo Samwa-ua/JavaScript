7c20a7eb807a46ffcc570a263f774f7f7751c613
Andriy Skokovskiy <askokovskiy@gmail.com>
2020-05-11 13:26:12 +0300

    challenge 1

--- srcript.js
+++ srcript.js
@@ -1,1 +1,23 @@
 console.log('Hello World');
+    
+var teamJohn = "Team John wins with a score ";
+var teamMike = "Team Mike wins with a score ";
+var teamMary = "Team Mary wins with a score ";
+var scoreJohn = ((116+960+123)/3);
+var scoreMike = ((116+900+123)/3);
+var scoreMary = ((116+96+165)/3);
+    if (scoreMary && scoreJohn < scoreMike) {
+        console.log(teamMike + Math.round(scoreMike));
+    }
+    else if (scoreMike && scoreJohn < scoreMary) {
+        console.log(teamMary + Math.round(scoreMary));
+    }
+    else if (scoreMike && scoreMary < scoreJohn) {
+        console.log(teamJohn + Math.round(scoreJohn));
+    }
+    else if (scoreJohn === scoreMike === scoreMary){
+        console.log("It`s a draw!");
+    }
+    else {
+        console.log("Something went wrong")
+    }
